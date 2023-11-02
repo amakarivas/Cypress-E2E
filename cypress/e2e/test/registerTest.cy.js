cf85b499955dd55667bd6e71887cbcb18cba13a6
amakarivas <59210780+amakarivas@users.noreply.github.com>
2023-11-01 21:10:08 -0700

    updated files

--- cypress/e2e/test/registerTest.cy.js
+++ cypress/e2e/test/registerTest.cy.js
@@ -1,19 +1,16 @@
 import { registerPage } from "../../pages/registerPage";
-const registerObj = new registerPage()
-import registerData from "../../fixtures/registerData.json"
+const registerObj = new registerPage();
+import registerData from "../../fixtures/registerData.json";
 describe("Registeration", () => {
-
-    it("register", () => {
-      registerObj.openURL()
-      registerObj.enterFirstName(registerData.firstName)
-      registerObj.enterLastName(registerData.lastName)
-      registerObj.enterEmail(registerData.email)
-      registerObj.enterTelephone(registerData.telephone)
-      registerObj.enterPassword(registerData.password, registerData.confirmPassword)
-      //registerObj.enterPassword(registerData.confirmPassword)
-      registerObj.selectPolicy()
-      registerObj.enterContinue()
-
-
-    });
+  it("register", () => {
+    registerObj.openURL();
+    registerObj.enterFirstName(registerData.firstName);
+    registerObj.enterLastName(registerData.lastName);
+    registerObj.enterEmail(registerData.email);
+    registerObj.enterTelephone(registerData.telephone);
+    registerObj.enterPassword(registerData.password,registerData.confirmPassword);
+    //registerObj.enterPassword(registerData.confirmPassword)
+    registerObj.selectPolicy();
+    registerObj.enterContinue();
   });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('')
+    cy.get('#input-email').type(email)
+    cy.get('#input-password').type(password)
+    cy.get("input[value='Login']").click()
+})
